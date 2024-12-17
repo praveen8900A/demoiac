38b53f3ae5a5b14a503bb3d3e8110a4f44139eae
praveen8900A <praveenkumardpmtn@gmail.com>
2024-12-17 23:41:24 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-west-1" # Specify your AWS region
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "iacdemobckt" # Updated bucket name
+  acl    = "private"
+
+  tags = {
+    Name        = "iacdemobckt"
+    Environment = "Development"
+  }
+}
